6415a1965c948e1c895c062c7d991041e77e6888
William Merussi <wmerussi@gmail.com>
2023-11-15 22:54:48 -0300

    Adding absolute path for components and interfaces, and installing fonts, typescript and router packages

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,18 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['expo-router/babel']
+    plugins: [
+      'expo-router/babel',
+      [
+        'module-resolver',
+        {
+          alias: {
+            '@components': './src/components',
+            '@interfaces': './src/interfaces',
+          },
+          extensions: [".js", ".jsx", ".tsx", ".ts"],
+        },
+      ],
+    ],
   };
 };
